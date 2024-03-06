280ce9119dd1833f75746e9953bca9f00f35e1ed
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2024-03-06 21:50:16 +0600

    [Done] Project Init

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
-  api.cache(true);
-  const plugins = [];
+    api.cache(true);
+    const plugins = [];
 
-  return {
-    presets: ['babel-preset-expo'],
+    return {
+        presets: ['babel-preset-expo'],
 
-    plugins,
-  };
+        plugins,
+    };
 };
